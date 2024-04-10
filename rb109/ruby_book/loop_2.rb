9ab1e14d1fb1a3de687b9c58a58cbc34c2469245
Joshua Hall <SEJoshuaHall@gmail.com>
2024-04-09 20:23:04 -0500

    test

---
+++ rb109/ruby_book/loop_2.rb
@@ -0,0 +1,7 @@
+puts "Do you want me to stop? If so, type STOP! Else type NO: "
+input = gets.chomp
+while input != "STOP" do
+  puts "OK, I'll continue..."
+  puts "Do you want me to stop? If so, type STOP! Else type NO: "
+  input = gets.chomp
+end
