df3b9a46eaa590249ce818867538e6a09a3a7357
SEJoshuaHall <sejoshuahall@gmail.com>
2024-05-20 19:31:07 -0500

    5/20/2024

--- rb110/test.rb
+++ rb110/test.rb
@@ -1,1 +1,11 @@
-manipulating arrays and hashes. For example, an exercise might be "reverse an array without using the built-in Array#reverse method", or "select the element out of the array if its index is a Fibonacci number," or "write a method that removes every other element from an array"
+def pangram?(string)
+  alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+  alpha_hsh = alphabet.to_h {|e| [e, false]}
+  string.chars.each do |char|
+    alpha_hsh[char] = true
+  end
+  alpha_hsh.values.all?
+end
+
+p pangram?("The quick brown fox jumps over the lazy dog.")
+p pangram?("This is not a pangram.")
