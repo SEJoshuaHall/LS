fcd172d6ace6046ce97a206f060e2510587935a5
Joshua Hall <sejoshuahall@gmail.com>
2024-06-15 08:23:24 -0500

    homeword 6_14_2024

---
+++ rb119/TopCoderInterviews/pairwise.rb
@@ -0,0 +1,90 @@
+=begin
+Given an array `arr`, find element pairs whose sum equal the second argument `arg` and return the sum of their indices.
+
+You may use multiple pairs that have the same numeric elements but different indices. Each pair should use the lowest possible available indices. Once an element has been used it cannot be reused to pair with another element. For instance, `pairwise([1, 1, 2], 3)` creates a pair `[2, 1]` using the 1 at index 0 rather than the 1 at index 1, because 0+2 < 1+2.
+
+For example `pairwise([7, 9, 11, 13, 15], 20)` returns `6`. The pairs that sum to 20 are `[7, 13]` and `[9, 11]`. We can then write out the array with their indices and values.
+
+|Index|0|1|2|3|4|
+|---|---|---|---|---|---|
+|Value|7|9|11|13|15|
+
+Below we'll take their corresponding indices and add them.
+
+7 + 13 = 20 → Indices 0 + 3 = 3  
+9 + 11 = 20 → Indices 1 + 2 = 3  
+3 + 3 = 6 → Return `6`
+
+PROBLEM
+1. find pairs whose sums == second arg
+  must be uniq elements
+  find the 1st index num for each element in pairs
+  sum their indices and return
+
+DATA
+Input(array, int)
+output integer
+
+ALG
+dup input array
+set var pairs
+iterate over dup input array
+  iterate over remaining
+    if sum == input int
+    concat pair to pairs
+    replace with '!'
+map and sort all pairs subs 
+select unique and flatten pairs
+set var selected []
+iterate over pairs
+  iterate over input array with index
+  if match and selected does not include index
+    concat index to selected
+sum selected and return
+=end
+#### Tests
+def pairwise(array, int)
+  return 0 if array.empty?
+  arr = array.dup
+  pairs = []
+  selected_indices = []
+  
+  arr.each_with_index do |num1, current_idx|
+    next if selected_indices.include?(current_idx)
+    ((current_idx + 1)..(array.length - 1)).each do |comp_idx|
+      if selected_indices.include?(comp_idx) || current_idx == comp_idx
+        p 'test'
+        next
+      end
+      if num1 + arr[comp_idx] == int
+        pairs << [num1, arr[comp_idx]]
+        selected_indices << current_idx
+        selected_indices << comp_idx
+      end
+    end
+  end
+
+  p selected_indices
+
+  # pairs.map! {|sub| sub.sort}
+  # pairs = pairs.uniq.flatten
+  # selected_indices = []
+  # arr = array.dup
+  # pairs.each do |num1|
+  #   arr.each_with_index do |num2, idx|
+  #     if num1 == num2 && selected_indices.none?(idx)
+  #       selected_indices << idx
+  #       break
+  #     end
+  #   end
+  # end
+  # selected_indices.inject(:+)
+end
+
+ pairwise([1, 2, 3, 3, 4, 4, 5, 6], 7)
+
+# p pairwise([1, 4, 2, 3, 0, 5], 7) == 11
+# p pairwise([1, 3, 2, 4], 4) == 1
+# p pairwise([1, 1, 1], 2) == 1
+# p pairwise([0, 0, 0, 0, 1, 1], 1) == 10
+# p pairwise([], 100) == 0
--- rb119/TopCoderInterviews/permalone.rb
+++ rb119/TopCoderInterviews/permalone.rb
@@ -1,179 +1,4 @@
 =begin
-Find the Symmetric Difference
-
----
-
-The mathematical term symmetric difference (`△` or `⊕`) of two sets is the set of elements which are in either of the two sets but not in both. For example, for sets `A = {1, 2, 3}` and `B = {2, 3, 4}`, `A △ B = {1, 4}`.
-
-Symmetric difference is a binary operation, which means it operates on only two elements. So to evaluate an expression involving symmetric differences among _three_ elements (`A △ B △ C`), you must complete one operation at a time. Thus, for sets `A` and `B` above, and `C = {2, 3}`, `A △ B △ C = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}`.
-
----
-
-Create a function that takes two or more arrays and returns an array of their symmetric difference. The returned array must contain only unique values (_no duplicates_).
-
-=end
-
-=begin
-
-=== PROBLEM ===
-
-Write a method that returns the symmetric difference of 2 or more input arrays
-
-Rules:
-  - symmetric difference: unique values that are in either input but not all
-  - returned array must be unique values
-
-=== EXAMPLES ===
-
-sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) == [1, 4, 5]
-  [1, 2, 5]△[2, 3, 5] --> [1, 3]
-  [1, 3]△[3, 4, 5] --> [1, 4, 5] 
-
-=== DATA ===
-
-input: any number of arrays of integers that need to be compared
-intermediate:
-  - arrays
-  - comparison (what elements are shared)
-  - recursion
-output: an array of unique integers that are the symmetric diffence of the input arrays
-
-=== ALGORITHM ===
-
-INITIALIZE `result` to reference an empty array
-FIND the unique integers in the first array
-  - Iterate over arr1 with #each
-  - If current element is not included in arr2, then add to result
-FIND the unique integers in the second array
-  - Iterate over arr2 with #each
-  - If current element is not included in arr1, then add to result
-REPEATE with `result` until there are no more input arrays 
-  - Update arr1 to `result`
-
-=end
-
-# def sym(*arr_s)
-#   arrs = [*arr_s]
-#   arr1 = arrs.shift
-#   result = []
-
-#   until arrs.empty?
-#     arr2 = arrs.shift
-    
-#     arr1.each do |num|
-#       result << num if !arr2.include?(num)
-#     end
-
-#     arr2.each do |num|
-#       result << num if !arr1.include?(num)
-#     end
-    
-#     arr1 = result
-#     result = []
-#   end
-
-#   arr1.sort.uniq
-# end
-
-# ## Tests
-# p sym([1, 2, 3], [5, 2, 1, 4]) == [3, 4, 5]
-# p sym([1, 2, 3, 3], [5, 2, 1, 4]) == [3, 4, 5]
-# p sym([1, 2, 3], [5, 2, 1, 4, 5]) == [3, 4, 5]
-# p sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) == [1, 4, 5]
-# p sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) == [2, 3, 4, 6, 7]
-# p sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) == [1, 2, 4, 5, 6, 7, 8, 9]
-
-
-------------------------------------------------------
-# PROBLEM
-# Find the difference between two input arrays, then the difference between that result and the next input array, until all input arrays have been compared. Return the resulting array.
-
-# EXAMPLES
-# Verified my understanding.
-# Rules:
-# Binary operation
-# Sort final product
-# Only unique numbers in return for each binary operation
-
-# DATA
-# Input: set of arrays
-# Output: result array
-
-# ALGORITHM
-# write a method which takes 2 or more arrays as arguments called sym
-# set array 1 to arr1, and array 2 to arr2
-# set current index to 1
-# loop
-#   get difference between arr1 and arr2 and set to arr1
-#   break if current index is length of input array - 1
-#   increment current index
-# return arr1
-
-# =end
-
-# def sym(*args)
-#   arr1, arr2 = args[0], args[1]
-#   current_index = 1
-#   loop do
-#     arr1 = ((arr1 - arr2) + (arr2 - arr1)).uniq.sort
-#     current_index += 1
-#     break if current_index >= args.count
-#     arr2 = args[current_index]
-#   end
-#   arr1
-# end
-
-
-
-# p sym([1, 2, 3], [5, 2, 1, 4]) == [3, 4, 5]
-# p sym([1, 2, 3, 3], [5, 2, 1, 4]) == [3, 4, 5]
-# p sym([1, 2, 3], [5, 2, 1, 4, 5]) == [3, 4, 5]
-# p sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) == [1, 4, 5]
-# p sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) == [2, 3, 4, 6, 7]
-# p sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) == [1, 2, 4, 5, 6, 7, 8, 9]
-
-
-
-# def sym(*arr_s)
-#   arrs = [*arr_s]
-#   arr1 = arrs.shift
-#   result = []
-  
-#   until arrs.empty?
-#     arr2 = arrs.shift
-
-#     arr1.each do |num|
-#       result << num if !arr2.include?(num)
-#     end
-
-#     arr2.each do |num|
-#       result << num if !arr1.include?(num)
-#     end
-
-#     arr1 = result
-#   end
-
-#   result.sort
-# end
-
-# ## Tests
-# p sym([1, 2, 3], [5, 2, 1, 4]) == [3, 4, 5]
-# p sym([1, 2, 3, 3], [5, 2, 1, 4]) == [3, 4, 5]
-# p sym([1, 2, 3], [5, 2, 1, 4, 5]) == [3, 4, 5]
-# p sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) == [1, 4, 5]
-# p sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) == [2, 3, 4, 6, 7]
-# p sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) == [1, 2, 4, 5, 6, 7, 8, 9]
-
-
------------------------------------------------------
-
-
-
-
-=begin
-No Repeats Please
-
----
 
 Return the number of total permutations of the provided string that don't have repeated consecutive letters. Assume that all characters in the provided string are each unique.
 
@@ -199,9 +24,6 @@ array of letter counts greater than 1
 output integer
 
 ALG
-if length is 1, return 1
-elsif only 1 character return 0
-else
 set combos
       HELPER get combos
         get all possible combinations
@@ -216,52 +38,55 @@ set combos
           concat
           repeat for length of string
 set qualifying
-HELPER
-find qualifying
-  loop over combos
-    if no 2 consecutive are the same concat
-      switch to true
-      iterate over chars with index
-        next if first
-        if any are the same as previous
-          switch = false
-    if switch true concat
-set multipliers array
-HELPER
-  get uniq chars from input
-  iterate
-    if count > 1
-      concat count to multipliers array
-take count of qualifying, multiply by each multiplier and return the result
+          HELPER
+          find qualifying
+            loop over combos
+              if no 2 consecutive are the same concat
+                switch to true
+                iterate over chars with index
+                  next if first
+                  if any are the same as previous
+                    switch = false
+              if switch true concat
+return count
 
-=end
+
 def get_combos(str)
   combos = []
-  arr = str.chars
-  counter = str.length
-  current = str.length
-  combos << arr.join
-  (1..counter).each do |start|
-    
-  end
+  str.chars.permutation(str.length) {|permutation| combos << permutation }
   combos
 end
 
-def find_qualifying(combos)
-
+def get_qualifying(combos)
+  result = []
+  combos.each do |arr|
+    qualifies = true
+    arr.each_with_index do |char, idx|
+      next if idx == 0
+      if char == arr[idx - 1]
+        qualifies = false
+      end
+    end
+    result << arr if qualifies
+  end
+  result
 end
-
-def permAlone(str)
   
+def permAlone(str)
+  combos = get_combos(str)
+  qualifying = get_qualifying(combos)
+  qualifying.count
 end
+
 
 p permAlone("aab") == 2
-p permAlone("aaa") == 0
 p permAlone("aabb") == 8
-p permAlone("abcdefa") = 3600
+p permAlone("abcdefa") == 3600
 p permAlone("abfdefa") == 2640
-p permAlone("zzzzzzzz") == 0
 p permAlone("a") == 1
 p permAlone("aaab") == 0
 p permAlone("aaabb") == 12
+p permAlone("zzzzzzzz") == 0
+p permAlone("aaa") == 0
 
+=end
