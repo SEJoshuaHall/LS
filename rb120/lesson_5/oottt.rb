d13990c3e044b2ba75e7461967c3f229d76d3f2f
[Joshua Hall] <[sejoshuahall@gmail.com]>
2024-07-09 16:11:03 -0500

    update dev branch to work on TTT

--- rb120/lesson_5/oottt.rb
+++ rb120/lesson_5/oottt.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-# Responsible for board state
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
@@ -30,7 +27,9 @@ class Board
   def winning_marker
     WINNING_LINES.each do |line|
       squares = @squares.values_at(*line)
-      return squares.first.marker if three_identical_markers?(squares)
+      if three_identical_markers?(squares)
+        return squares.first.marker
+      end
     end
     nil
   end
@@ -40,40 +39,36 @@ class Board
   end
 
   # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/MethodLength
   def draw
-    puts '     |     |'
+    puts "     |     |"
     puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
-    puts '     |     |'
-    puts '-----+-----+-----'
-    puts '     |     |'
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
     puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
-    puts '     |     |'
-    puts '-----+-----+-----'
-    puts '     |     |'
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
     puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
-    puts '     |     |'
+    puts "     |     |"
   end
   # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/MethodLength
 
   private
 
   def three_identical_markers?(squares)
     markers = squares.select(&:marked?).collect(&:marker)
     return false if markers.size != 3
-
     markers.min == markers.max
   end
 end
 
-# Keeps track of state of squares
 class Square
-  INITIAL_MARKER = ' '
+  INITIAL_MARKER = " "
 
   attr_accessor :marker
 
-  def initialize(marker = INITIAL_MARKER)
+  def initialize(marker=INITIAL_MARKER)
     @marker = marker
   end
 
@@ -90,7 +85,6 @@ class Square
   end
 end
 
-# Manages player moves
 class Player
   attr_reader :marker
 
@@ -99,10 +93,9 @@ class Player
   end
 end
 
-# Main game engine
 class TTTGame
-  HUMAN_MARKER = 'X'
-  COMPUTER_MARKER = 'O'
+  HUMAN_MARKER = "X"
+  COMPUTER_MARKER = "O"
   FIRST_TO_MOVE = HUMAN_MARKER
 
   attr_reader :board, :human, :computer
@@ -129,7 +122,6 @@ class TTTGame
       player_move
       display_result
       break unless play_again?
-
       reset
       display_play_again_message
     end
@@ -139,18 +131,17 @@ class TTTGame
     loop do
       current_player_moves
       break if board.someone_won? || board.full?
-
       clear_screen_and_display_board if human_turn?
     end
   end
 
   def display_welcome_message
-    puts 'Welcome to Tic Tac Toe!'
-    puts ''
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
   end
 
   def display_goodbye_message
-    puts 'Thanks for playing Tic Tac Toe! Goodbye!'
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
 
   def clear_screen_and_display_board
@@ -164,9 +155,9 @@ class TTTGame
 
   def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
-    puts ''
+    puts ""
     board.draw
-    puts ''
+    puts ""
   end
 
   def human_moves
@@ -175,7 +166,6 @@ class TTTGame
     loop do
       square = gets.chomp.to_i
       break if board.unmarked_keys.include?(square)
-
       puts "Sorry, that's not a valid choice."
     end
 
@@ -201,9 +191,9 @@ class TTTGame
 
     case board.winning_marker
     when human.marker
-      puts 'You won!'
+      puts "You won!"
     when computer.marker
-      puts 'Computer won!'
+      puts "Computer won!"
     else
       puts "It's a tie!"
     end
@@ -212,18 +202,17 @@ class TTTGame
   def play_again?
     answer = nil
     loop do
-      puts 'Would you like to play again? (y/n)'
+      puts "Would you like to play again? (y/n)"
       answer = gets.chomp.downcase
-      break if %w[y n].include? answer
-
-      puts 'Sorry, must be y or n'
+      break if %w(y n).include? answer
+      puts "Sorry, must be y or n"
     end
 
     answer == 'y'
   end
 
   def clear
-    system 'clear'
+    system "clear"
   end
 
   def reset
@@ -234,7 +223,7 @@ class TTTGame
 
   def display_play_again_message
     puts "Let's play again!"
-    puts ''
+    puts ""
   end
 end
 
