c422ba7f7977e653876093a75568eccc550f0d3e
Joshua Hall <sejoshuahall@gmail.com>
2024-12-17 15:14:44 -0600

    12/17/2024

--- rb120/lesson_4/easy_3.rb
+++ rb120/lesson_4/easy_3.rb
@@ -1,4 +1,56 @@
 =begin
 
 1.
+class Greeting
+  def greet(message)
+    puts message
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet("Hello")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet("Goodbye")
+  end
+end
+
+hello = Hello.new
+hello.hi #STDOUT 'Hello'
+
+# hello = Hello.new
+# hello.bye #no method error
+
+# hello = Hello.new
+# hello.greet #Wrong number of arguments error
+
+# hello = Hello.new
+# hello.greet("Goodbye") #STDOUT 'Goodbye'
+
+# Hello.hi #No method error
+
+2.
 =end
+
+require 'minitest/autorun'
+
+class Car
+  def initialize(t, y, c)
+    @type = t
+    @year = y
+    @color = c
+  end
+
+  def 
+
+end
+
+
+
+assert_equal
+assert_same
+assert_raises
