b37943340ea2d6fecb6eb1576263168348269167
Joshua Michael Hall <sejoshuahall@gmail.com>
2024-12-28 15:18:40 -0600

    linked_list 2nd version

--- rb130/challenges/medium/simple_linked_list.rb
+++ rb130/challenges/medium/simple_linked_list.rb
@@ -1,20 +1,20 @@
-
+# Creates elements for the linked lists.
 class Element
   attr_reader :datum, :next
 
-  def initialize(datum, next_datum=nil)
+  def initialize(datum, next_element=nil)
     @datum = datum
-    @next = next_datum
+    @next = next_element
   end
 
   def tail?
-    self.next == nil
+    @next == nil
   end
 end
 
-
+# Linked list objects to hold elements.
 class SimpleLinkedList
-  attr_reader :list
+  attr_reader :head
 
   def initialize
     @list = []
@@ -25,39 +25,51 @@ class SimpleLinkedList
   end
 
   def empty?
-    @list.count == 0
+    @list.empty?
   end
 
-  def push(datum)
-    @list << Element.new(datum, @list.last)
+  def push(arg)
+    @list << Element.new(arg, head)
   end
 
   def peek
-    
+    return nil if @list.empty?
+    @list[-1].datum
   end
 
   def head
-
-  end
-
-  def next
-
+    return nil if empty?
+    @list[-1]
   end
 
   def pop
+    return nil if empty?
+    @list.pop.datum
+  end
 
+  def self.from_a(array)
+    class_list = SimpleLinkedList.new
+    unless array == nil
+      array.each_with_index do |e, i|
+        class_list.list << (Element.new(e, (class_list.list.size > 1 ? class_list.list[-1] : nil)))
+      end
+    end
+    class_list
   end
 
   def to_a
-
+    @list.map { |element| element.datum }
   end
 
   def reverse
-
+    @list.reverse
+    @list.each_with_index do |e, i|
+      if i == @list.count - 1
+        e.next = nil
+      else
+        e.next = @list[i + 1]
+      end
+    end
   end
-
-  def self.from_a(array) #create complete list from array
-
-  end
-  
 end
+
