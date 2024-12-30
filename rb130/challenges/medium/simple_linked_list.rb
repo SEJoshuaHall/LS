134d7f036b2decaeef90e49e0dbf3e923ae7676b
Joshua Michael Hall <sejoshuahall@gmail.com>
2024-12-30 10:23:41 -0600

    completed simple_linked_list

--- rb130/challenges/medium/simple_linked_list.rb
+++ rb130/challenges/medium/simple_linked_list.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 # Creates elements for the linked lists.
 class Element
   attr_reader :datum, :next
 
-  def initialize(datum, next_element=nil)
+  def initialize(datum, next_element = nil)
     @datum = datum
     @next = next_element
   end
 
   def tail?
-    @next == nil
+    @next.nil?
   end
 end
 
@@ -17,39 +19,62 @@ class SimpleLinkedList
   attr_reader :head
 
   def size
-    
+    counter = 0
+    return 0 if @head.nil?
+
+    counter += 1
+    current = head
+    until current.next.nil?
+      counter += 1
+      current = current.next
+    end
+
+    counter
   end
 
   def empty?
-    
+    @head.nil?
   end
 
   def push(arg)
-    
+    @head = if head.nil?
+              Element.new(arg)
+            else
+              Element.new(arg, @head)
+            end
   end
 
   def peek
-    
-  end
-
-  def head
-    
+    @head&.datum
   end
 
   def pop
-    
+    result = @head
+    @head = @head.next
+    result.datum
   end
 
   def self.from_a(array)
-    
+    if [nil, []].include?(array)
+      SimpleLinkedList.new
+    else
+      list = SimpleLinkedList.new
+      array.reverse.each { |datum| list.push(datum) }
+      list
+    end
   end
 
   def to_a
-    
+    result = []
+    head = @head
+    until head.nil?
+      result << head.datum
+      head = head.next
+    end
+    result
   end
 
   def reverse
-  
+    SimpleLinkedList.from_a(to_a.reverse)
   end
 end
-
--- rb130/challenges/medium/test.rb
+++ rb130/challenges/medium/test.rb
@@ -1,23 +1,74 @@
-require 'date'
+class Element
+  attr_reader :datum, :next
 
-# Set your target weekday (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
-weekday = 1  # Monday
+  def initialize(datum, next_element = nil)
+    @datum = datum
+    @next = next_element
+  end
 
-# Set your target year and month
-year = 2024
-month = 12
-
-# Get all dates in the month
-first_day = Date.new(year, month, 1)
-last_day = Date.new(year, month, -1)  # -1 gets the last day of the month
-
-# Find all matching weekdays
-matching_days = (first_day..last_day).select do |date|
-  date.wday == weekday
+  def tail?
+    @next.nil?
+  end
 end
 
-# Print results
-matching_days.each do |date|
-  puts date.strftime("%A, %B %d, %Y")
-end
+class SimpleLinkedList
+  attr_reader :head
 
+  def size
+    size = 0
+    current_elem = @head
+    while (current_elem)
+      size += 1
+      current_elem = current_elem.next
+    end
+    size
+  end
+
+  def empty?
+    @head.nil?
+  end
+
+  def push(datum)
+    element = Element.new(datum, @head)
+    @head = element
+  end
+
+  def peek
+    @head.datum if @head
+  end
+
+  def pop
+    datum = peek
+    new_head = @head.next
+    @head = new_head
+    datum
+  end
+
+  def self.from_a(array)
+    array = [] if array.nil?
+
+    list = SimpleLinkedList.new
+    array.reverse_each { |datum| list.push(datum) }
+    list
+  end
+
+  def to_a
+    array = []
+    current_elem = head
+    while !current_elem.nil?
+      array.push(current_elem.datum)
+      current_elem = current_elem.next
+    end
+    array
+  end
+
+  def reverse
+    list = SimpleLinkedList.new
+    current_elem = head
+    while !current_elem.nil?
+      list.push(current_elem.datum)
+      current_elem = current_elem.next
+    end
+    list
+  end
+end
